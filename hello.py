95990810a014810479b120643b6dd95318eb85c8
Bruno Correa <brunosantos6ft@gmail.com>
2025-02-05 09:35:52 -0300

    first commit

---
+++ hello.py
@@ -0,0 +1,50 @@
+import airportsdata
+import json
+
+airportsdata = airportsdata.load('IATA')
+
+def generate_sql_updates():
+    print(airportsdata['JFK'])
+    
+    with open('nameless-airports.json') as airports_json:
+        airports = json.load(airports_json)
+        
+        sql_updates = []
+        manual_sql_updates = []
+
+        for airport in airports:
+            airport_iata = airport['iata']
+            airport_info = airportsdata.get(airport_iata)
+            
+            if not airport_info:
+                manual_updates = []
+                if airport['city_name'] is None:
+                    print(f"City name for {airport_iata} not found, Airport id: {airport['id']}")
+                    manual_updates.append("city_name = ''")
+                if airport['country_name'] is None:
+                    print(f"Country name for {airport_iata} not found, Airport id: {airport['id']}")
+                    manual_updates.append("country_name = ''") 
+                
+                if manual_updates:
+                    sql = f"UPDATE public.airports SET {', '.join(manual_updates)} WHERE id = {airport['id']};"
+                    manual_sql_updates.append(sql)
+                continue
+            
+            updates = []
+            if airport['city_name'] is None:
+                updates.append(f"city_name = '{airport_info['city']}'")
+            if airport['country_name'] is None:
+                updates.append(f"country_name = '{airport_info['country']}'")
+            
+            if updates:
+                sql = f"UPDATE public.airports SET {', '.join(updates)} WHERE id = {airport['id']};"
+                sql_updates.append(sql)
+        
+        with open('update_airports.sql', 'w') as sql_file:
+            sql_file.write("\n".join(sql_updates))
+        
+        with open('manual_update_airports.sql', 'w') as manual_sql_file:
+            manual_sql_file.write("\n".join(manual_sql_updates))
+        
+        print("SQL update scripts generated")
+generate_sql_updates()
---
+++ manual_update_airports.sql
@@ -0,0 +1,20 @@
+-- Updating country_name for airports
+UPDATE public.airports SET country_name = 'France' WHERE id = 2794;
+UPDATE public.airports SET country_name = 'Indonesia' WHERE id = 1919;
+UPDATE public.airports SET country_name = 'China' WHERE id = 2723;
+UPDATE public.airports SET country_name = 'Papua New Guinea' WHERE id = 1087;
+UPDATE public.airports SET country_name = 'Brazil' WHERE id = 2733;
+UPDATE public.airports SET country_name = 'United Kingdom' WHERE id = 1695;
+UPDATE public.airports SET country_name = 'Monaco' WHERE id = 2261;
+UPDATE public.airports SET country_name = 'Oman' WHERE id = 1423;
+UPDATE public.airports SET country_name = 'Saudi Arabia' WHERE id = 2744;
+UPDATE public.airports SET country_name = 'British Virgin Islands' WHERE id = 1907;
+UPDATE public.airports SET country_name = 'British Virgin Islands' WHERE id = 2107;
+UPDATE public.airports SET country_name = 'South Korea' WHERE id = 1709;
+UPDATE public.airports SET country_name = 'France' WHERE id = 2787;
+UPDATE public.airports SET country_name = 'Bahamas' WHERE id = 3111;
+UPDATE public.airports SET city_name = 'Paracatu', country_name = 'Brazil' WHERE id = 5344;
+UPDATE public.airports SET city_name = 'Apuí', country_name = 'Brazil' WHERE id = 5351;
+UPDATE public.airports SET city_name = 'Sobral', country_name = 'Brazil' WHERE id = 5346;
+UPDATE public.airports SET city_name = ' São Benedito', country_name = 'Brazil' WHERE id = 5353;
+UPDATE public.airports SET country_name = 'British Virgin Islands' WHERE id = 3286;
