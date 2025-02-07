9f91f06a68c1556d16d3503a0ff503dd6090a0bf
Bruno Correa <brunosantos6ft@gmail.com>
2025-02-07 17:27:03 -0300

    chore: format

--- update_airports.sql
+++ update_airports.sql
@@ -1,260 +1,52 @@
-UPDATE public.airports 
-                SET country_name = 'EG' 
-                WHERE id = 38 
-                AND iata = 'UVL' 
-                AND name = 'UVL';
-UPDATE public.airports 
-                SET country_name = 'BS' 
-                WHERE id = 2272 
-                AND iata = 'MAY' 
-                AND name = 'MAY-Clarence A. Bain';
-UPDATE public.airports 
-                SET country_name = 'IN' 
-                WHERE id = 1273 
-                AND iata = 'DIB' 
-                AND name = 'DIB';
-UPDATE public.airports 
-                SET country_name = 'PE' 
-                WHERE id = 1875 
-                AND iata = 'RIJ' 
-                AND name = 'RIJ-Juan Simons Vela';
-UPDATE public.airports 
-                SET country_name = 'KR' 
-                WHERE id = 196 
-                AND iata = 'CHF' 
-                AND name = 'CHF';
-UPDATE public.airports 
-                SET country_name = 'BS' 
-                WHERE id = 1639 
-                AND iata = 'WTD' 
-                AND name = 'WTD';
-UPDATE public.airports 
-                SET country_name = 'US' 
-                WHERE id = 2681 
-                AND iata = 'SCC' 
-                AND name = 'SCC/MUSC';
-UPDATE public.airports 
-                SET country_name = 'MD' 
-                WHERE id = 202 
-                AND iata = 'BZY' 
-                AND name = 'BZY-Aeroporto Internacional de Leadoveni';
-UPDATE public.airports 
-                SET country_name = 'CO' 
-                WHERE id = 2644 
-                AND iata = 'CPB' 
-                AND name = 'CPB/SKCA Narcisa Navas de Capurgana';
-UPDATE public.airports 
-                SET country_name = 'ID' 
-                WHERE id = 129 
-                AND iata = 'LAH' 
-                AND name = 'LAH';
-UPDATE public.airports 
-                SET country_name = 'MY' 
-                WHERE id = 2249 
-                AND iata = 'MEP' 
-                AND name = 'MEP';
-UPDATE public.airports 
-                SET country_name = 'VG' 
-                WHERE id = 2131 
-                AND iata = 'NGD' 
-                AND name = 'NGD-Auguste George';
-UPDATE public.airports 
-                SET country_name = 'MY' 
-                WHERE id = 1692 
-                AND iata = 'SWY' 
-                AND name = 'SWY';
-UPDATE public.airports 
-                SET country_name = 'GW' 
-                WHERE id = 1453 
-                AND iata = 'BQE' 
-                AND name = 'BQE';
-UPDATE public.airports 
-                SET country_name = 'CN' 
-                WHERE id = 1573 
-                AND iata = 'AOG' 
-                AND name = 'AOG-Base Aérea de Anshan';
-UPDATE public.airports 
-                SET country_name = 'CV' 
-                WHERE id = 1656 
-                AND iata = 'NTO' 
-                AND name = 'NTO';
-UPDATE public.airports 
-                SET country_name = 'IL' 
-                WHERE id = 1218 
-                AND iata = 'EIY' 
-                AND name = 'EIY';
-UPDATE public.airports 
-                SET country_name = 'JP' 
-                WHERE id = 1898 
-                AND iata = 'RBJ' 
-                AND name = 'RBJ';
-UPDATE public.airports 
-                SET country_name = 'CA' 
-                WHERE id = 2699 
-                AND iata = 'YLB' 
-                AND name = 'YLB';
-UPDATE public.airports 
-                SET country_name = 'FJ' 
-                WHERE id = 1664 
-                AND iata = 'LUC' 
-                AND name = 'LUC';
-UPDATE public.airports 
-                SET country_name = 'MY' 
-                WHERE id = 2464 
-                AND iata = 'KGU' 
-                AND name = 'KGU';
-UPDATE public.airports 
-                SET country_name = 'JP' 
-                WHERE id = 988 
-                AND iata = 'HTR' 
-                AND name = 'HTR';
-UPDATE public.airports 
-                SET country_name = 'CH' 
-                WHERE id = 629 
-                AND iata = 'GVA' 
-                AND name = 'GVA/LSGG, Geneva, Switzerland';
-UPDATE public.airports 
-                SET country_name = 'PH' 
-                WHERE id = 2798 
-                AND iata = 'IAO' 
-                AND name = 'IAO-Sayak';
-UPDATE public.airports 
-                SET city_name = 'Reyes Acozac', country_name = 'MX' 
-                WHERE id = 5342 
-                AND iata = 'NLU' 
-                AND name = 'Aeroporto Internacional General Felipe Ángeles';
-UPDATE public.airports 
-                SET city_name = 'Araripina', country_name = 'BR' 
-                WHERE id = 5343 
-                AND iata = 'JAW' 
-                AND name = 'Aeroporto Araripina';
-UPDATE public.airports 
-                SET city_name = 'Iguatu', country_name = 'BR' 
-                WHERE id = 5352 
-                AND iata = 'QIG' 
-                AND name = 'Aeroporto Iguatu';
-UPDATE public.airports 
-                SET city_name = 'Costa Marques', country_name = 'BR' 
-                WHERE id = 5355 
-                AND iata = 'CQS' 
-                AND name = 'Aeroporto de Costa Marques';
-UPDATE public.airports 
-                SET city_name = 'Lages', country_name = 'BR' 
-                WHERE id = 5362 
-                AND iata = 'LAJ' 
-                AND name = 'Aeroporto Federal Antônio Correia Pinto de Macedo';
-UPDATE public.airports 
-                SET country_name = 'BR' 
-                WHERE id = 5371 
-                AND iata = 'SSZ' 
-                AND name = 'SSZ/ SBST';
-UPDATE public.airports 
-                SET city_name = 'Teofilo Otoni', country_name = 'BR' 
-                WHERE id = 5345 
-                AND iata = 'TFL' 
-                AND name = 'Aeroporto Teófilo Otoni';
-UPDATE public.airports 
-                SET city_name = 'Sulaymaniyah', country_name = 'IQ' 
-                WHERE id = 5347 
-                AND iata = 'ISU' 
-                AND name = 'Aeroporto Internacional de Sulaimani';
-UPDATE public.airports 
-                SET city_name = 'Arbil', country_name = 'IQ' 
-                WHERE id = 5348 
-                AND iata = 'EBL' 
-                AND name = 'Aeroporto Internacional de Erbil';
-UPDATE public.airports 
-                SET city_name = 'Basrah', country_name = 'IQ' 
-                WHERE id = 5349 
-                AND iata = 'BSR' 
-                AND name = 'Aeroporto Internacional de Basrah';
-UPDATE public.airports 
-                SET city_name = 'Ariquemes', country_name = 'BR' 
-                WHERE id = 5356 
-                AND iata = 'AQM' 
-                AND name = 'Aeroporto de Ariquemes';
-UPDATE public.airports 
-                SET city_name = 'Salinopolis', country_name = 'BR' 
-                WHERE id = 5358 
-                AND iata = 'OPP' 
-                AND name = 'Aeroporto Regional de Salinopolis';
-UPDATE public.airports 
-                SET country_name = 'KH' 
-                WHERE id = 2865 
-                AND iata = 'RBE' 
-                AND name = 'RBE-Ratanakiri';
-UPDATE public.airports 
-                SET city_name = 'Chitose / Tomakomai', country_name = 'JP' 
-                WHERE id = 5360 
-                AND iata = 'CTS' 
-                AND name = 'Aeroporto de Crateús - Dr. Lúcio Lima';
-UPDATE public.airports 
-                SET country_name = 'BR' 
-                WHERE id = 5363 
-                AND iata = 'TOW' 
-                AND name = 'TOW/ SBTD';
-UPDATE public.airports 
-                SET country_name = 'IN' 
-                WHERE id = 3149 
-                AND iata = 'WGC' 
-                AND name = 'WGC';
-UPDATE public.airports 
-                SET country_name = 'BR' 
-                WHERE id = 5372 
-                AND iata = 'JJD' 
-                AND name = 'JJD/ SBJE';
-UPDATE public.airports 
-                SET country_name = 'MY' 
-                WHERE id = 3283 
-                AND iata = 'TPG' 
-                AND name = 'TPG';
-UPDATE public.airports 
-                SET city_name = 'Eirunepe', country_name = 'BR' 
-                WHERE id = 5698 
-                AND iata = 'ERN' 
-                AND name = 'Eirunepe, Brazil';
-UPDATE public.airports 
-                SET city_name = 'Porto Alegre', country_name = 'BR' 
-                WHERE id = 5339 
-                AND iata = 'QNS' 
-                AND name = 'Base Aérea de Canoas';
-UPDATE public.airports 
-                SET city_name = 'Tulum', country_name = 'MX' 
-                WHERE id = 5340 
-                AND iata = 'TQO' 
-                AND name = 'Aeroporto Internacional de Tulum Felipe Carrillo Puerto';
-UPDATE public.airports 
-                SET city_name = 'Baghdad', country_name = 'IQ' 
-                WHERE id = 5350 
-                AND iata = 'BGW' 
-                AND name = 'Aeroporto Internacional de Bagdad';
-UPDATE public.airports 
-                SET city_name = 'Pimenta Bueno', country_name = 'BR' 
-                WHERE id = 5354 
-                AND iata = 'PBQ' 
-                AND name = 'Aeroporto Pimenta Bueno';
-UPDATE public.airports 
-                SET country_name = 'VU' 
-                WHERE id = 3342 
-                AND iata = 'TAH' 
-                AND name = 'TAH';
-UPDATE public.airports 
-                SET city_name = 'Paranavai', country_name = 'BR' 
-                WHERE id = 5357 
-                AND iata = 'PVI' 
-                AND name = 'Aeroporto Municipal de Paranavaí';
-UPDATE public.airports 
-                SET city_name = 'Garanhuns', country_name = 'BR' 
-                WHERE id = 5359 
-                AND iata = 'QGP' 
-                AND name = 'Aeroporto de Garanhuns';
-UPDATE public.airports 
-                SET city_name = 'Teixeira De Freitas', country_name = 'BR' 
-                WHERE id = 5341 
-                AND iata = 'TXF' 
-                AND name = 'Aeroporto de Teixeira de Freitas';
-UPDATE public.airports 
-                SET city_name = 'Manicore', country_name = 'BR' 
-                WHERE id = 5361 
-                AND iata = 'MNX' 
-                AND name = 'Aeroporto de Manicoré';
+UPDATE public.airports SET country_name = 'EG' WHERE id = 38 AND iata = 'UVL' AND name = 'UVL';
+UPDATE public.airports SET country_name = 'BS' WHERE id = 2272 AND iata = 'MAY' AND name = 'MAY-Clarence A. Bain';
+UPDATE public.airports SET country_name = 'IN' WHERE id = 1273 AND iata = 'DIB' AND name = 'DIB';
+UPDATE public.airports SET country_name = 'PE' WHERE id = 1875 AND iata = 'RIJ' AND name = 'RIJ-Juan Simons Vela';
+UPDATE public.airports SET country_name = 'KR' WHERE id = 196 AND iata = 'CHF' AND name = 'CHF';
+UPDATE public.airports SET country_name = 'BS' WHERE id = 1639 AND iata = 'WTD' AND name = 'WTD';
+UPDATE public.airports SET country_name = 'US' WHERE id = 2681 AND iata = 'SCC' AND name = 'SCC/MUSC';
+UPDATE public.airports SET country_name = 'MD' WHERE id = 202 AND iata = 'BZY' AND name = 'BZY-Aeroporto Internacional de Leadoveni';
+UPDATE public.airports SET country_name = 'CO' WHERE id = 2644 AND iata = 'CPB' AND name = 'CPB/SKCA Narcisa Navas de Capurgana';
+UPDATE public.airports SET country_name = 'ID' WHERE id = 129 AND iata = 'LAH' AND name = 'LAH';
+UPDATE public.airports SET country_name = 'MY' WHERE id = 2249 AND iata = 'MEP' AND name = 'MEP';
+UPDATE public.airports SET country_name = 'VG' WHERE id = 2131 AND iata = 'NGD' AND name = 'NGD-Auguste George';
+UPDATE public.airports SET country_name = 'MY' WHERE id = 1692 AND iata = 'SWY' AND name = 'SWY';
+UPDATE public.airports SET country_name = 'GW' WHERE id = 1453 AND iata = 'BQE' AND name = 'BQE';
+UPDATE public.airports SET country_name = 'CN' WHERE id = 1573 AND iata = 'AOG' AND name = 'AOG-Base Aérea de Anshan';
+UPDATE public.airports SET country_name = 'CV' WHERE id = 1656 AND iata = 'NTO' AND name = 'NTO';
+UPDATE public.airports SET country_name = 'IL' WHERE id = 1218 AND iata = 'EIY' AND name = 'EIY';
+UPDATE public.airports SET country_name = 'JP' WHERE id = 1898 AND iata = 'RBJ' AND name = 'RBJ';
+UPDATE public.airports SET country_name = 'CA' WHERE id = 2699 AND iata = 'YLB' AND name = 'YLB';
+UPDATE public.airports SET country_name = 'FJ' WHERE id = 1664 AND iata = 'LUC' AND name = 'LUC';
+UPDATE public.airports SET country_name = 'MY' WHERE id = 2464 AND iata = 'KGU' AND name = 'KGU';
+UPDATE public.airports SET country_name = 'JP' WHERE id = 988 AND iata = 'HTR' AND name = 'HTR';
+UPDATE public.airports SET country_name = 'CH' WHERE id = 629 AND iata = 'GVA' AND name = 'GVA/LSGG, GenevSwitzerland';
+UPDATE public.airports SET country_name = 'PH' WHERE id = 2798 AND iata = 'IAO' AND name = 'IAO-Sayak';
+UPDATE public.airports SET city_name = 'Reyes Acozac', country_name = 'MX' WHERE id = 5342 AND iata = 'NLU' AND name = 'Aeroporto Internacional General Felipe Ángeles';
+UPDATE public.airports SET city_name = 'Araripina', country_name = 'BR' WHERE id = 5343 AND iata = 'JAW' AND name = 'Aeroporto Araripina';
+UPDATE public.airports SET city_name = 'Iguatu', country_name = 'BR' WHERE id = 5352 AND iata = 'QIG' AND name = 'Aeroporto Iguatu';
+UPDATE public.airports SET city_name = 'Costa Marques', country_name = 'BR' WHERE id = 5355 AND iata = 'CQS' AND name = 'Aeroporto de Costa Marques';
+UPDATE public.airports SET city_name = 'Lages', country_name = 'BR' WHERE id = 5362 AND iata = 'LAJ' AND name = 'Aeroporto Federal Antônio Correia Pinto de Macedo';
+UPDATE public.airports SET country_name = 'BR' WHERE id = 5371 AND iata = 'SSZ' AND name = 'SSZ/ SBST';
+UPDATE public.airports SET city_name = 'Teofilo Otoni', country_name = 'BR' WHERE id = 5345 AND iata = 'TFL' AND name = 'Aeroporto Teófilo Otoni';
+UPDATE public.airports SET city_name = 'Sulaymaniyah', country_name = 'IQ' WHERE id = 5347 AND iata = 'ISU' AND name = 'Aeroporto Internacional de Sulaimani';
+UPDATE public.airports SET city_name = 'Arbil', country_name = 'IQ' WHERE id = 5348 AND iata = 'EBL' AND name = 'Aeroporto Internacional de Erbil';
+UPDATE public.airports SET city_name = 'Basrah', country_name = 'IQ' WHERE id = 5349 AND iata = 'BSR' AND name = 'Aeroporto Internacional de Basrah';
+UPDATE public.airports SET city_name = 'Ariquemes', country_name = 'BR' WHERE id = 5356 AND iata = 'AQM' AND name = 'Aeroporto de Ariquemes';
+UPDATE public.airports SET city_name = 'Salinopolis', country_name = 'BR' WHERE id = 5358 AND iata = 'OPP' AND name = 'Aeroporto Regional de Salinopolis';
+UPDATE public.airports SET country_name = 'KH' WHERE id = 2865 AND iata = 'RBE' AND name = 'RBE-Ratanakiri';
+UPDATE public.airports SET city_name = 'Chitose / Tomakomai', country_name = 'JP' WHERE id = 5360 AND iata = 'CTS' AND name = 'Aeroporto de Crateús - Dr. Lúcio Lima';
+UPDATE public.airports SET country_name = 'BR' WHERE id = 5363 AND iata = 'TOW' AND name = 'TOW/ SBTD';
+UPDATE public.airports SET country_name = 'IN' WHERE id = 3149 AND iata = 'WGC' AND name = 'WGC';
+UPDATE public.airports SET country_name = 'BR' WHERE id = 5372 AND iata = 'JJD' AND name = 'JJD/ SBJE';
+UPDATE public.airports SET country_name = 'MY' WHERE id = 3283 AND iata = 'TPG' AND name = 'TPG';
+UPDATE public.airports SET city_name = 'Eirunepe', country_name = 'BR' WHERE id = 5698 AND iata = 'ERN' AND name = 'Eirunepe, Brazil';
+UPDATE public.airports SET city_name = 'Porto Alegre', country_name = 'BR' WHERE id = 5339 AND iata = 'QNS' AND name = 'Base Aérea de Canoas';
+UPDATE public.airports SET city_name = 'Tulum', country_name = 'MX' WHERE id = 5340 AND iata = 'TQO' AND name = 'Aeroporto Internacional de Tulum Felipe Carrillo Puerto';
+UPDATE public.airports SET city_name = 'Baghdad', country_name = 'IQ' WHERE id = 5350 AND iata = 'BGW' AND name = 'Aeroporto Internacional de Bagdad';
+UPDATE public.airports SET city_name = 'Pimenta Bueno', country_name = 'BR' WHERE id = 5354 AND iata = 'PBQ' AND name = 'Aeroporto Pimenta Bueno';
+UPDATE public.airports SET country_name = 'VU' WHERE id = 3342 AND iata = 'TAH' AND name = 'TAH';
+UPDATE public.airports SET city_name = 'Paranavai', country_name = 'BR' WHERE id = 5357 AND iata = 'PVI' AND name = 'Aeroporto Municipal de Paranavaí';
+UPDATE public.airports SET city_name = 'Garanhuns', country_name = 'BR' WHERE id = 5359 AND iata = 'QGP' AND name = 'Aeroporto de Garanhuns';
+UPDATE public.airports SET city_name = 'Teixeira De Freitas', country_name = 'BR' WHERE id = 5341 AND iata = 'TXF' AND name = 'Aeroporto de Teixeira de Freitas';
+UPDATE public.airports SET city_name = 'Manicore', country_name = 'BR' WHERE id = 5361 AND iata = 'MNX' AND name = 'Aeroporto de Manicoré';
--- update_airports_manual_homol.sql
+++ update_airports_manual_homol.sql
@@ -1,89 +1,15 @@
-UPDATE public.airports
-SET city_name = 'Tortola', country_name = 'British Virgin Islands'
-WHERE id = 3684
-AND iata = 'TOV'
-AND name = 'TOV-Base Marítima de West End';
-
-UPDATE public.airports
-SET city_name = 'Paradise Island', country_name = 'Bahamas'
-WHERE id = 3480
-AND iata = 'WZY'
-AND name = 'WZY-Base Marítima de Paradise Island';
-
-UPDATE public.airports
-SET city_name = 'Valence', country_name = 'France'
-WHERE id = 3067
-AND iata = 'XHK'
-AND name = 'XHK-SNCF Valence TGV Train Station';
-
-UPDATE public.airports
-SET city_name = 'Bar-le-Duc', country_name = 'France'
-WHERE id = 3058
-AND iata = 'XBD'
-AND name = 'XBD-Bar Le Duc SNCF Train Station';
-
-UPDATE public.airports
-SET city_name = 'Dammam', country_name = 'Saudi Arabia'
-WHERE id = 3012
-AND iata = 'DMS'
-AND name = 'DMS-Estação de Ônibus Saptco';
-
-UPDATE public.airports
-SET city_name = 'Itumbiara', country_name = 'Brazil'
-WHERE id = 3001
-AND iata = 'ITR'
-AND name = 'ITR';
-
-UPDATE public.airports
-SET city_name = 'Wenshan', country_name = 'China'
-WHERE id = 2989
-AND iata = 'WNH'
-AND name = 'WNH';
-
-UPDATE public.airports
-SET city_name = 'Monaco', country_name = 'Monaco'
-WHERE id = 2666
-AND iata = 'MCM'
-AND name = 'MCM-Heliporto de Mônaco';
-
-UPDATE public.airports
-SET city_name = 'North Sound', country_name = 'British Virgin Islands'
-WHERE id = 2468
-AND iata = 'NSX'
-AND name = 'NSX';
-
-UPDATE public.airports
-SET city_name = 'Purwokerto', country_name = 'Indonesia'
-WHERE id = 2245
-AND iata = 'PWL'
-AND name = 'PWL';
-
-UPDATE public.airports
-SET city_name = 'Road Town', country_name = 'British Virgin Islands'
-WHERE id = 2231
-AND iata = 'RAD'
-AND name = 'RAD-Base Marítima de Road Town';
-
-UPDATE public.airports
-SET city_name = 'Samchok', country_name = 'South Korea'
-WHERE id = 1994
-AND iata = 'SUK'
-AND name = 'SUK';
-
-UPDATE public.airports
-SET city_name = 'Swindon', country_name = 'United Kingdom'
-WHERE id = 1980
-AND iata = 'SWI'
-AND name = 'SWI-Estação ferroviária de Swindon';
-
-UPDATE public.airports
-SET city_name = 'Dibaa', country_name = 'Oman'
-WHERE id = 1637
-AND iata = 'BYB'
-AND name = 'BYB';
-
-UPDATE public.airports
-SET city_name = 'Garuahi', country_name = 'Papua New Guinea'
-WHERE id = 1218
-AND iata = 'GRH'
-AND name = 'GRH';
+UPDATE public.airports SET city_name = 'Tortola', country_name = 'British Virgin Islands' WHERE id = 3684 AND iata = 'TOV' AND name = 'TOV-Base Marítima de West End';
+UPDATE public.airports SET city_name = 'Paradise Island', country_name = 'Bahamas' WHERE id = 3480 AND iata = 'WZY' AND name = 'WZY-Base Marítima de Paradise Island';
+UPDATE public.airports SET city_name = 'Valence', country_name = 'France' WHERE id = 3067 AND iata = 'XHK' AND name = 'XHK-SNCF Valence TGV Train Station';
+UPDATE public.airports SET city_name = 'Bar-le-Duc', country_name = 'France' WHERE id = 3058 AND iata = 'XBD' AND name = 'XBD-Bar Le Duc SNCF Train Station';
+UPDATE public.airports SET city_name = 'Dammam', country_name = 'Saudi Arabia' WHERE id = 3012 AND iata = 'DMS' AND name = 'DMS-Estação de Ônibus Saptco';
+UPDATE public.airports SET city_name = 'Itumbiara', country_name = 'Brazil' WHERE id = 3001 AND iata = 'ITR' AND name = 'ITR';
+UPDATE public.airports SET city_name = 'Wenshan', country_name = 'China' WHERE id = 2989 AND iata = 'WNH' AND name = 'WNH';
+UPDATE public.airports SET city_name = 'Monaco', country_name = 'Monaco' WHERE id = 2666 AND iata = 'MCM' AND name = 'MCM-Heliporto de Mônaco';
+UPDATE public.airports SET city_name = 'North Sound', country_name = 'British Virgin Islands' WHERE id = 2468 AND iata = 'NSX' AND name = 'NSX';
+UPDATE public.airports SET city_name = 'Purwokerto', country_name = 'Indonesia' WHERE id = 2245 AND iata = 'PWL' AND name = 'PWL';
+UPDATE public.airports SET city_name = 'Road Town', country_name = 'British Virgin Islands' WHERE id = 2231 AND iata = 'RAD' AND name = 'RAD-Base Marítima de Road Town';
+UPDATE public.airports SET city_name = 'Samchok', country_name = 'South Korea' WHERE id = 1994 AND iata = 'SUK' AND name = 'SUK';
+UPDATE public.airports SET city_name = 'Swindon', country_name = 'United Kingdom' WHERE id = 1980 AND iata = 'SWI' AND name = 'SWI-Estação ferroviária de Swindon';
+UPDATE public.airports SET city_name = 'Dibaa', country_name = 'Oman' WHERE id = 1637 AND iata = 'BYB' AND name = 'BYB';
+UPDATE public.airports SET city_name = 'Garuahi', country_name = 'Papua New Guinea' WHERE id = 1218 AND iata = 'GRH' AND name = 'GRH';
